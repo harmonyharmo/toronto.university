5a4e20b504c2386ea9694ccec7ebd0677ccc3119
Thomas Hu <hytqqq@gmail.com>
2017-09-04 01:05:45 -0400

    successfully searches course description

--- src/app/core/search/search.service.ts
+++ src/app/core/search/search.service.ts
@@ -4,33 +4,16 @@ import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
 import { Http, Response } from '@angular/http';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Injectable()
 export class SearchService {
-  searchTerms = new Subject();
-  query: string;
-  members: any[];
   results: string;
 
   constructor(private http: HttpClient) {
   }
 
   search(query) {
-    // console.log(this.http.get(
-    //   'https://timetable.iit.artsci.utoronto.ca/api/20179/courses?org=&code=' + this.query +
-    //   '&section=&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title='))
-    // console.log(
-    //   this.http.get(
-    //     'https://timetable.iit.artsci.utoronto.ca/api/20179/courses?org=&code=csc108' +
-    //     '&section=&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title='));
-    // console.log(this.http.get(`https://api.github.com/orgs/angular/members?page=1&per_page=5`) // (4)
-    //   .map(res => res.json()) // (5)
-    //   .subscribe(data => {
-    //     if (data) this.members = data; // (6)
-    //   })
-    // )
-    // ;
-// set params to go to URL
 
     // this.heroes$ = this.searchTerms
     //   .debounceTime(300)        // wait for 300ms pause in events
@@ -38,13 +21,21 @@ export class SearchService {
     //   .switchMap(term => Observable.create(term.toLowerCase());
 
     console.log(query);
+    if (query.length < 6) {
+      return;
+    }
+
     this.http.get('https://cors-anywhere.herokuapp.com/' // use proxy to bypass CORS
-      + 'https://timetable.iit.artsci.utoronto.ca/api/20179/courses?org=&code=csc165'
+      + 'https://timetable.iit.artsci.utoronto.ca/api/20179/courses?org=&code='
+      + query
       + '&section=&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=')
       .subscribe(
         data => {
-          this.results = JSON.stringify(data);
-          console.log(data['courseDescription']);
+          for (const course of Object.keys(data)) {
+            this.results = data[course].courseDescription;
+            console.log(this.results);
+            return;
+          }
         },
         (err: HttpErrorResponse) => {
           if (err.error instanceof Error) {
