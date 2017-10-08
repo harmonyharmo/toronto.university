4eb804e3ced6aab2618f98d252fa5f7e7669b5f3
Thomas Hu <hytqqq@gmail.com>
2017-10-08 18:43:14 -0400

    added room search

--- src/app/core/search/search.service.ts
+++ src/app/core/search/search.service.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/map';
 import { Http, RequestOptions, Response } from '@angular/http';
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { URLSearchParams } from '@angular/http';
+import { isUndefined, log } from 'util';
 
 
 @Injectable()
@@ -16,8 +17,8 @@ export class SearchService {
   constructor(private http: HttpClient) {
   }
 
-  private get_link(display: string, link: string) {
-    return '<a href=' + link + '>' + display + '</a>';
+  private static get_link(display: string, link: string) {
+    return '<a href="' + link + '">' + display + '</a>';
   }
 
   search(query: string) {
@@ -30,9 +31,13 @@ export class SearchService {
     if (query.length === 2) {
       this.searchBuilding();
     }
-    if (query.length >= 6) {
+    if (query.length === 6) {
       this.searchCourse();
     }
+    // unstable
+    // if (this.query.length >= 5) {
+    //   this.searchRoom();
+    // }
   }
 
   private searchCourse() {
@@ -46,6 +51,9 @@ export class SearchService {
         .subscribe(
           data => {
             const fall = data[Object.keys(data)[0]];
+            if (isUndefined(fall)) {
+              return;
+            }
             const meetings = fall.meetings;
             const meeting = meetings[Object.keys(meetings)[0]];
 
@@ -54,10 +62,11 @@ export class SearchService {
 
             if (this.query.startsWith('csc')) {
               const link = 'https://markus.teach.cs.toronto.edu/' + this.query + '-2017-09/en/assignments';
-              this.results = this.get_link('Markus', link) + this.results;
+              this.results = SearchService.get_link('Markus', link) + this.results;
             }
           },
           (err: HttpErrorResponse) => {
+            this.results = '';
             if (err.error instanceof Error) {
               // A client-side or network error occurred. Handle it accordingly.
               console.log('An error occurred:', err.error.message);
@@ -101,7 +110,7 @@ export class SearchService {
         data => {
           const building = data[0];
           const link = 'https://www.google.com/maps/place/' + encodeURIComponent(building.name);
-          this.results = this.get_link(building.name, link);
+          this.results = SearchService.get_link(building.name, link);
         },
         (err: HttpErrorResponse) => {
           if (err.error instanceof Error) {
@@ -116,4 +125,26 @@ export class SearchService {
   }
 
 
+  private searchRoom() {
+    // const match = this.query.match(/[a-z]+|\d+/ig);
+    // const link = `http://www.osm.utoronto.ca/class_spec/f?p=210:1:::::P1_BLDG,P1_ROOM:${match[0].toUpperCase()},${match[1]}`;
+    const link = `http://www.osm.utoronto.ca/i/Photos/Website/RoomPlansPDF/${this.query}.pdf`;
+    this.http.get('https://cors-anywhere.herokuapp.com/' // use proxy to bypass CORS
+      + link,
+      {headers: this.headers})
+      .subscribe(
+        data => {
+          this.results = SearchService.get_link('Room Info', link);
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.log('An error occurred:', err.error.message);
+          } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+          }
+        });
+  }
 }
