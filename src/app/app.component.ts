1e2a1db55b4baff23361350aaea528a1c4025216
Thomas Hu <hytqqq@gmail.com>
2018-01-06 10:58:44 -0500

    avoid duplication in html with Ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  sidenav_links = [
+    {url: '', icon: 'home', name: 'Home'},
+    {url: 'referral', icon: 'person_add', name: 'Referral'},
+    {url: 'contact', icon: 'email', name: 'Contact'},
+    {url: 'about', icon: 'help_outline', name: 'About'},
+  ];
+
+  constructor() {
+  }
 }
--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  cards = [
+    {link: 'guide', title: 'User Guide'},
+    {link: 'about', title: 'About'},
+    {link: 'join', title: 'Join Us'},
+  ];
 
   constructor() {
   }
