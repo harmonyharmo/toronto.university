ac4884fac618fc8096629cca8263fbbdabf23060
monkeykingg <zhihong.wang@mail.utoronto.ca>
2018-01-26 17:42:56 -0500

    test commit

--- src/app/content/team/team.component.ts
+++ src/app/content/team/team.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TeamComponent implements OnInit {
 
   names = [
-    'Wednesday', 'Zidong Xie','Muyuan Cao'
+    'Wednesday', 'Zidong Xie','Muyuan Cao', 'Bill Wang'
   ];
 
   constructor() {
