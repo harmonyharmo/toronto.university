506b166da3e8d11c44575599834645bbd9841e22
PYyu6 <35384999+PYyu6@users.noreply.github.com>
2018-01-27 20:26:42 -0500

    Update team.component.ts

--- src/app/content/team/team.component.ts
+++ src/app/content/team/team.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TeamComponent implements OnInit {
 
   names = [
-    'Wednesday',
+    'Wednesday', 'Pwiyu'
   ];
 
   constructor() {
